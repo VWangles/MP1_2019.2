386dec3084e13d005358abab970888f145c68dc5
vwangles <victor.wangles@hotmail.com>
2019-10-03 23:25:30 -0300

    teste

--- velha.cpp
+++ velha.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include <stdexcept>
+#include "velha.hpp"
 
+using namespace std;
+
+int testaVelha(int *velhaMatriz) {
+return 1;
+}
--- velha.hpp
+++ velha.hpp
@@ -1,1 +1,10 @@
+#ifndef HEADER_VELHA_H
+#define HEADER_VELHA_H
 
+#include <stdexcept>
+
+using namespace std;
+
+int testaVelha(int*);
+
+#endif // HEADER_VELHA_H
