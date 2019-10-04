0e1ce7333a26229bd16e4c06e4362b2f58055ee9
vwangles <victor.wangles@hotmail.com>
2019-10-03 23:21:18 -0300

    teste

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "velha.hpp"
 
+using namespace std;
+
+int main() {
+    int velha[9] = {2, 0, 1,
+                    0, 2, 0,
+                    1, 0, 1};
+
+switch(testaVelha(velha)) {
+    case 1: cout << "Vencedor foi X." << endl;
+                    break;
+    case 2: cout << "Vencedor foi O." << endl;
+                    break;
+    case 0: cout << "Jogo empatado." << endl;
+                    break;
+    case -1: cout << "Jogo esta indefinido." << endl;
+                    break;
+    case -2: cout << "Jogo impossivel." << endl;
+                    break;
+}
+
+return 0;
+}
